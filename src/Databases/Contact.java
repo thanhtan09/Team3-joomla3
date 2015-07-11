236b40cb6f445a63279a923bc859cf04da7df3bc
nga.nguyen <nga.nguyen@LGDN13031-W7C01.logigear.com>
2015-07-11 11:40:01 +0700

    Nga nguyen

---
+++ src/Databases/Contact.java
@@ -0,0 +1,58 @@
+package Databases;
+
+import java.util.Calendar;
+
+public class Contact {
+	String name;
+	String catetory;
+	String status;
+	String image;
+	
+	public Contact(){
+		this.name="";
+		this.catetory="";
+		this.status="";
+		this.image="";
+	};
+	
+	//Set title
+	public void setName(String _name){
+		Calendar now = Calendar.getInstance();
+		int day = now.get(Calendar.DAY_OF_MONTH);
+		int hour = now.get(Calendar.HOUR_OF_DAY);
+		int minute = now.get(Calendar.MINUTE);
+		int second = now.get(Calendar.SECOND);
+		String newName = "1"+day+hour+minute+second+"-"+_name;
+		this.name = newName;
+	}
+	
+	//Get title
+	public String getName(){
+		return name;
+	}
+	
+	//Set category
+	public void setCategory(String _cate){
+		this.catetory = _cate;
+		}
+		
+	//Get category
+	public String getCategory(){
+		return catetory;
+		}
+		
+	//Set status
+		public void setStatus(String _status){
+		this.status = _status;
+		}
+		
+	//Get status
+		public String getStatus(){
+		return status;
+		}
+	
+	//Get image
+		public String getImage(){
+			return image;
+		}
+}
