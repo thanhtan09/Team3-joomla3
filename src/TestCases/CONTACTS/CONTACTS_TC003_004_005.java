9c2203c3a7fa5d2bdf3e19536d4cca86e17e196a
nga.nguyen <nga.nguyen@LGDN13031-W7C01.logigear.com>
2015-07-11 16:55:13 +0700

    NN

---
+++ src/TestCases/CONTACTS/CONTACTS_TC003_004_005.java
@@ -0,0 +1,86 @@
+package TestCases.CONTACTS;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import Functions.Abstract_test;
+import Pages.Contacts_page;
+import Pages.Factory_page;
+import Pages.Home_page;
+import Pages.Login_page;
+
+public class CONTACTS_TC003_004_005 extends Abstract_test{
+	private Login_page loginPage;
+	private Home_page homePage;
+	private Contacts_page contactsPage;
+	
+	@BeforeClass
+	public void setup(){
+		driver = openJoomla();
+	}
+	
+	@Test(description = "Verify user can unpublish a published contact")
+	public void TC_CONTACT_004 (){
+		
+		log.info("Login with valid account");
+		loginPage = Factory_page.getLoginPage(driver);
+		homePage = loginPage.loginValidAccount(user.getUsername(), user.getPassword(),"");
+		
+		log.info("Enter Contacts page");
+		contactsPage = homePage.navigatetoContactspage();		
+		
+		log.info("Create new contact");
+		contactsPage.addNewContact(contact2.getName(),contact2.getCategory(),contact2.getStatus(),"","");
+		
+		log.info("Verify Contact successfully saved message is displayed");
+		verifyTrue(contactsPage.isMessageContactDisplay());
+		
+		log.info("Verify Created contact is displayed on the contacts table");
+		verifyTrue(contactsPage.isContactDisplay(contact2.getName()));
+		
+		log.info("UnPublish a publish contact");
+		contactsPage.unpublishContact(contact2.getName());
+		
+		log.info("Verify '1 contact successfully unpublished' message is displayed");
+		contactsPage.isMessageUnpublishContactDisplay();
+		
+		log.info("Verify the icon of the selected item is showed as 'Publish'");
+		contactsPage.isUnpublishContactDisplay(contact2.getName());
+		
+	}
+	
+	@Test(description = "Verify user can publish a published contact", dependsOnMethods = "TC_CONTACT_004")
+	public void TC_CONTACT_003 (){
+
+		log.info("Publish an unpublish contact");
+		contactsPage.publishContact(contact2.getName());
+		
+		log.info("Verify '1 contact successfully published' message is displayed");
+		contactsPage.isMessagePublishContactDisplay();
+		
+		log.info("Verify the icon of the selected item is showed as 'Publish'");
+		contactsPage.isPublishContactDisplay(contact2.getName());
+					
+	}
+	
+	@Test(description = "Verify user can move a contact to the archive", dependsOnMethods = "TC_CONTACT_003")
+	public void TC_CONTACT_005 (){
+
+		log.info("Archive a contact");
+		contactsPage.archiveContact(contact2.getName());
+		
+		log.info("Verify '1 contact archived' message is displayed");
+		contactsPage.isMessageArchivedContactDisplay();
+		
+		log.info("Verify the archived contact is displayed on the table grid");
+		contactsPage.isArchivedContactDisplay(contact2.getName());
+		
+	}
+	
+	@AfterClass
+	public void end(){
+		contactsPage.deleteContact(contact2.getName());
+		shutdown();
+	}
+}
