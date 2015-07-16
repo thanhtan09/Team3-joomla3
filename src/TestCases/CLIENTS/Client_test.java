a37bfc79face283b81f683f4ceeda00c9e5dd048
Victor Vo <Victor Vo@VictorVo>
2015-07-16 23:56:17 +0700

    Tan Vo

--- src/TestCases/CLIENTS/Client_test.java
+++ src/TestCases/CLIENTS/Client_test.java
@@ -12,56 +12,32 @@ public class Client_test extends Abstract_test{
 	private Login_page loginPage;
 	private Home_page homePage;
 	private Client_page clientPage;
-	private NewClient_page newclientPage;
 	
 	@BeforeClass
 	public void setup(){
 		driver = openJoomla();
 	}
 	
-	@Test (description = "Verify that user can create a new client")
-	public void TC_CLIENTS_001(){
+	@Test (description = "Verify that user can browse Banner client help page")
+	public void TC_CLIENTS_007(){
 		log.info("Login with valid account");
 		loginPage = Factory_page.getLoginPage(driver);
 		homePage = loginPage.loginValidAccount(user.getUsername(), user.getPassword(),"");
 		
-		log.info("Create new client");
+		log.info("Navigate to CLient page");
 		clientPage = homePage.navigatetoCLientpage();
-		clientPage.addNewClient(client.getName(), client.getContact(), client.getEmail(), "", "Save");		
-		newclientPage = Factory_page.getNewClientPage(driver);
 		
-		log.info("A message : Client successfully saved shows and edit client page displays");
-		verifyTrue(clientPage.isMessageDisplay(),"Client successfully saved");
-		verifyTrue(newclientPage.isEditClientPage(), "Edit client page displays");
-		
+		log.info("Banner client help page appears");
+		verifyTrue(clientPage.isHelpPage(), "Banner client help page appears");
 	}
 	
-	@Test (description = "Verify that user can edit a client")
-	public void TC_CLIENTS_002(){
+	@Test (description = "Verify that user can search a client  by using filter textbox")
+	public void TC_CLIENTS_008(){
 		
-		log.info("Edit client");
-		newclientPage.addClient(client2.getName(), client2.getContact(), client2.getEmail(), "", "");
-		
-		log.info("A message : Client successfully saved shows and Client is edited");
-		clientPage = Factory_page.getClientPage(driver);
-		verifyTrue(clientPage.isMessageDisplay(),"Client successfully saved");
-		verifyTrue(clientPage.isClientcreated(client2.getName()),"New client is created and matched with entered data");
-	}
-	
-	@Test (description = "Verify that user can publish a client")
-	public void TC_CLIENTS_003(){
-		
-		log.info("Publish a client");
-		clientPage.publishClient(client2.getName());
-		
-		log.info("A message : 1 client successfully published shows and Client is published");
-		verifyTrue(clientPage.isMessagePublishedClientDisplay(), "1 client successfully published");
-		verifyTrue(clientPage.isClientPublished(client2.getName()), "Client is published");
 	}
 	
 	@AfterClass
-	public void end(){	
-		clientPage.deleteClient(client2.getName());
+	public void end(){
 		shutdown();
 	}
 }
