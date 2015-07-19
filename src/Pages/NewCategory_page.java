1d6523a698142236f8d29a9bb49dff61b4837f6c
giang <giang.nguyen@logigear.com>
2015-07-19 09:19:20 +0700

    gggg

--- src/Pages/NewCategory_page.java
+++ src/Pages/NewCategory_page.java
@@ -14,4 +14,20 @@ public class NewCategory_page extends Abstract_page{
 		enter(driver, By.xpath(Interfaces.NewCatetoryPage.TXT_TITLE), title);
 		click(driver, By.xpath(Interfaces.NewCatetoryPage.BTN_SAVEANDCLOSE));
 	}
+	
+	public void editCategory (String title, String button){
+		enter(driver, By.xpath(Interfaces.NewCatetoryPage.TXT_TITLE), title);
+		
+		if (button.equals("Save")){
+			click(driver, By.xpath(Interfaces.NewCatetoryPage.BTN_SAVE));
+		}
+		
+		else if (button.equals("Cancel")){
+			click(driver, By.xpath(Interfaces.NewCatetoryPage.BTN_CLOSE));
+		}
+		
+		else {
+			click(driver, By.xpath(Interfaces.NewCatetoryPage.BTN_SAVEANDCLOSE));
+		}
+	}
 }
