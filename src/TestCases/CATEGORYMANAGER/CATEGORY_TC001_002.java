20a0cd304d0949b13ba1d0c3f65eea64f21eb2be
giang <giang.nguyen@logigear.com>
2015-07-20 21:28:29 +0700

    ggg

---
+++ src/TestCases/CATEGORYMANAGER/CATEGORY_TC001_002.java
@@ -0,0 +1,67 @@
+package TestCases.CATEGORYMANAGER;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import Functions.Abstract_test;
+import Pages.Categories_page;
+import Pages.Factory_page;
+import Pages.Home_page;
+import Pages.Login_page;
+import Pages.NewCategory_page;
+import Pages.WebLinks_page;
+
+public class CATEGORY_TC001_002 extends Abstract_test{
+	private Login_page loginPage;
+	private Home_page homePage;
+	private Categories_page categoriesPage;
+	private NewCategory_page newcatePage;
+	
+	@BeforeClass
+	public void setup(){
+		driver = openJoomla();
+	}
+	
+	@Test(description = "Verify user can create new category with valid information")
+	public void TC_CATEGORY_001 (){
+		
+		log.info("Login with valid account");
+		loginPage = Factory_page.getLoginPage(driver);
+		homePage = loginPage.loginValidAccount(user.getUsername(), user.getPassword(),"");
+		
+		log.info("Enter Category Manager page");
+		categoriesPage = homePage.navigatetoCategoryManagerpage();	
+		
+		log.info("Create new Category");
+		categoriesPage = categoriesPage.addNewCategory(category.getTitle());
+		
+		log.info("Verify message successful displays and category displays in table");
+		verifyTrue(categoriesPage.isMessageDisplay(), "Category is saved successful");
+		verifyTrue(categoriesPage.isCategoryDisplay(category.getTitle()), "Category is displayed in table");
+	}
+	
+	@Test(description = "Verify that user can edit a category", dependsOnMethods ="TC_CATEGORY_001")
+	public void TC_CATEGORY_002 (){
+		log.info("Edit a category");
+		newcatePage = categoriesPage.clickEdit(category.getTitle());
+		newcatePage.editCategory(category2.getTitle(), "Save");
+		
+		log.info("A message Category successfully saved shows and Edit an Aritcal Category page displays");
+		verifyTrue(newcatePage.isMessageSuccessDisplay());
+		verifyTrue(newcatePage.isEditPage());
+		
+		log.info("Click Save and Closed");
+		categoriesPage = newcatePage.clickSaveandClose();
+		
+		log.info("A message Category successfully saved shows and new category displays in table");
+		verifyTrue(categoriesPage.isMessageDisplay());
+		verifyTrue(categoriesPage.isCategoryDisplay(category2.getTitle()));
+		
+	}
+	@AfterClass
+	public void end(){
+		categoriesPage.deleteCategory(category2.getTitle());
+		shutdown();
+	}
+}
