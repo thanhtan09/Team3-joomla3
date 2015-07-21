5bd0a05bff13cc82071c044a616169f996845262
giang <giang.nguyen@logigear.com>
2015-07-21 22:31:21 +0700

    g

---
+++ src/TestCases/WEBLINKS/WEBLINKS_TC006.java
@@ -0,0 +1,64 @@
+package TestCases.WEBLINKS;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import Functions.Abstract_test;
+import Pages.Factory_page;
+import Pages.Home_page;
+import Pages.Login_page;
+import Pages.WebLinks_page;
+
+public class WEBLINKS_TC006 extends Abstract_test{
+	
+	private Login_page loginPage;
+	private Home_page homePage;
+	private WebLinks_page weblinkPage;
+	
+	@BeforeClass
+	public void setup(){
+		driver = openJoomla();
+	}
+	
+	@Test(description = "Verify user can check in a weblink with valid information")
+	public void TC_WEBLINK_006 (){
+		
+		log.info("Login with valid account");
+		loginPage = Factory_page.getLoginPage(driver);
+		homePage = loginPage.loginValidAccount(user.getUsername(), user.getPassword(),"");
+		
+		log.info("Enter Weblink page");
+		weblinkPage = homePage.navigatetoWeblinkpage();		
+		
+		log.info("Create new weblink");
+		weblinkPage.addNewWebLink(weblink1.getName(), weblink1.getUrl(), weblink1.getContent(), weblink1.getStatus(), "" , "Save");
+		
+		log.info("Verify message Weblink successfully saved displayed");
+		verifyTrue(weblinkPage.isMessageWeblinkDisplay(),"Weblink is created successfully");
+		
+		shutdown();
+		driver = openJoomla();
+		
+		log.info("Login with valid account");
+		loginPage = Factory_page.getLoginPage(driver);
+		homePage = loginPage.loginValidAccount(user.getUsername(), user.getPassword(),"");
+		
+		log.info("Enter Weblink page");
+		weblinkPage = homePage.navigatetoWeblinkpage();	
+		
+		log.info("Check in weblink");
+		weblinkPage.checkinWeblink(weblink1.getName());
+		
+		log.info("Weblink is checked in successfully");
+		verifyTrue(weblinkPage.isCheckinMessage());
+		verifyTrue(weblinkPage.isCheckinWeblink(weblink1.getName()));
+		
+	}
+	
+	@AfterClass
+	public void end(){
+		weblinkPage.deleteWeblink(weblink1.getName());
+		shutdown();
+	}
+}
